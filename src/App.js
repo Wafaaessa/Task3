1c11e38f21969545b72aa79377bb4707446d7549
Wafaaessa <Wafaaessa98@icloud.com>
2024-01-29 16:41:49 +0200

    task three

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserProfile from './Component/UserProfile';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+      <UserProfile/>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/Component/UserProfile.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+
+export default function UserProfile() {
+  
+  return (
+    <div>
+  <div className="container d-flex justify-content-center align-items-center ">
+             
+             <div className="card">
+     
+              <div className="upper">
+     
+              <img src="https://i.imgur.com/Qtrsrk5.jpg" className="img-fluid"/>
+
+                
+              </div>
+     
+              <div className="user text-center">
+     
+                <div className="profile">
+     
+                  <img src="https://i.imgur.com/JgYD2nQ.jpg" className="rounded-circle" width="80"/>
+                  
+                </div>
+     
+              </div>
+     
+     
+              <div className="mt-5 text-center">
+     
+                <h1 className="name fw-bolder">Harsh P</h1>
+                <p className="fw-bolder mt-3  btn btn-sm follow">Product Designer</p>
+             
+     
+     <div className="container-fluid">
+         <div className="row">
+             <div className="col-md-12 text-center mt-4 pt-3 ">
+     <p className="text-center">Crafting Seamless Experiences, Where UX Design Meets Business Efficiency!</p>
+             </div>
+         </div>
+     </div>
+     </div>
+     
+     <div className=" container  text-center ">
+     
+             <div className="col-md-12  d-flex justify-content-center align-items-center social ">
+             
+                 
+               <a  href=' https://twitter.com/?lang=ar' className="icon p-2 mx-2  justify-content-center align-items-center">
+               <i className="fa-brands fa-twitter mx-3 p-2 text-info"></i>
+                </a>
+            <a  href='https://www.behance.net/' className="icon p-2 mx-2  justify-content-center align-items-center">
+                <i className="fa-brands fa-behance mx-5 p-2 text-info"></i>
+                </a>
+                <a className="icon p-2 mx-0 justify-content-center align-items-center">
+                <i className="fa-solid fa-basketball text-danger mx-0 p-2"></i>
+                </a>
+              
+                 
+          
+              
+             </div>
+         </div>
+          
+            <div className="container ">
+             <div className="col-md-12 mt-3">
+              <h5>Create a Component: </h5>  
+                 <p>
+                     Create a functional component named UserProfile that displays the user's name, email, and profile picture.
+                     <br/>
+                     Additional: Use dummy data to display information.
+                 </p>
+             </div>
+            </div>   
+     
+     
+            
+             </div>
+     
+     
+     
+           </div>
+     
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
